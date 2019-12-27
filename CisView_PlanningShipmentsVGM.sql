8156acb2cdf5e33abd4b8ec5e638bc14a016685b
simmer7 <59197394+simmer7@users.noreply.github.com>
2019-12-27 16:32:55 +0100

    Added Coload, LastModified columns and extra join
    Join on TblData_ShipmentWhsHeader

--- CisView_PlanningShipmentsVGM.sql
+++ CisView_PlanningShipmentsVGM.sql
@@ -1,11 +1,21 @@
 CREATE VIEW [dbo].[CisView_PlanningShipmentsVGM]
 AS
-SELECT      dbo.TblData_Shipment.iShipment_id, 
-				dbo.TblData_Shipment.fid_FileId,
-				fid_FilePlanning,
-				cFilePlanningNr,
-				dbo.TblData_Shipment.cBnumber, dbo.TblData_Shipment.fVGM, 
-                dbo.TblData_Shipment.cVGMContact, dbo.TblData_Shipment.cVGMtype
+/* Admin: Jan Veenstra		Date: 10-12-2019		Change: Export 15 Coload */
+SELECT      TblData_Shipment.iShipment_id, 
+			TblData_Shipment.fid_FileId,
+			fid_FilePlanning,
+			cFilePlanningNr,
+			TblData_Shipment.cBnumber,
+			TblData_Shipment.fVGM, 
+			TblData_Shipment.cVGMContact,
+			TblData_Shipment.cVGMtype,
+			TblData_ShipmentWhsHeader.iShipmentWhsHdr,
+			TblData_ShipmentWhsHeader.fid_ColoadAddressId,
+			TblData_ShipmentWhsHeader.cColoadRemark,
+			TblData_ShipmentWhsHeader.dWhsInDate,
+			TblData_ShipmentWhsHeader.cLastModifiedBy,
+			TblData_ShipmentWhsHeader.dLastModified
 FROM        dbo.TblData_Shipment 
-                left outer join TblData_FilePlanning on fid_FilePlanning = iFilePlanningId
-			LEFT OUTER JOIN dbo.TblBase_GenCodes ON dbo.TblData_Shipment.cVGMtype = dbo.TblBase_GenCodes.cGenCode
+            left join TblData_FilePlanning on fid_FilePlanning = iFilePlanningId
+			LEFT JOIN TblBase_GenCodes ON TblData_Shipment.cVGMtype = dbo.TblBase_GenCodes.cGenCode
+			LEFT JOIN TblData_ShipmentWhsHeader ON TblData_Shipment.iShipment_id = dbo.TblData_ShipmentWhsHeader.fid_ShipmentId
